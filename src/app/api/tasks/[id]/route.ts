1f17d6cdeb39c76873323abd2357292910c4ecd4
Valeriy Svystun <valeriy.all@gmail.com>
2025-12-29 20:38:41 +0200

    fixed(route.ts): eslint-disable - no-unused-vars

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import clientPromise from "../../../../../lib/mongodb";
 import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
